c845924378716fc9bb920d5948cb12b045ce43b1
henrizin <henrilopes1@gmail.com>
2025-09-30 19:15:29 -0300

    Update

--- src/log_analyzer/api/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-API package initialization
-"""
-
-from .main import app
-from .models import AnalysisRequest, AnalysisResponse
-from .dependencies import get_analyzer
-
-__all__ = ["app", "AnalysisRequest", "AnalysisResponse", "get_analyzer"]
